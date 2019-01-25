9982067c6b2cddf49a2237c52ff3325df39c7096
kbrian97 <ktorgbede2015@fau.edu>
2019-01-24 23:27:39 -0500

    Classes finished just need to log

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,2 +1,31 @@
 // CODE here for your Lambda Classes
 
+class Person{
+    constructor(value) {
+        this.name = value.name;
+        this.age = value.age;
+        this.location = value.location;
+    }
+
+    speak() {
+        `Hello my name is ${this.name}, I am form ${this.location}`
+    }
+}
+
+class Instructor extends Person{
+    constructor(value){
+        super(value);
+        this.specialty = value.specialty;
+        this.favLanguage = value.favLanguage;
+        this.catchPharse = value.catchPharse;
+    }
+
+    demo(subject) {
+        return `Today we are learning about ${subject}`
+    }
+    grade(subject, student) {
+        return `${student} recieves a perfect score on ${subject}`
+    }
+}
+
+
